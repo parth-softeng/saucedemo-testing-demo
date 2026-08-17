942ba3a45dca2d45ad104d6437df1c9f1fe99d24
parth-softeng <parth.softeng@gmail.com>
2026-08-17 17:08:10 +0530

    test: add SauceDemo test user fixtures

---
+++ automation/fixtures/users.fixture.ts
@@ -0,0 +1,31 @@
+export const STANDARD_USER = {
+  username: 'standard_user',
+  password: 'secret_sauce',
+};
+
+export const LOCKED_OUT_USER = {
+  username: 'locked_out_user',
+  password: 'secret_sauce',
+};
+
+export const PROBLEM_USER = {
+  username: 'problem_user',
+  password: 'secret_sauce',
+};
+
+export const PERFORMANCE_GLITCH_USER = {
+  username: 'performance_glitch_user',
+  password: 'secret_sauce',
+};
+
+export const ERROR_USER = {
+  username: 'error_user',
+  password: 'secret_sauce',
+};
+
+export const VISUAL_USER = {
+  username: 'visual_user',
+  password: 'secret_sauce',
+};
+
+export const INVALID_PASSWORD = 'wrong_password';
